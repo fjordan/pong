0ff1a4b3733d83fa145bb97bc83563ab7aa34e14
Forrest Jordan <forrestjordan@gmail.com>
2012-04-13 12:40:21 -0500

    initial commit - stillbugs

--- pong/ViewController.h
+++ pong/ViewController.h
@@ -8,6 +8,30 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    IBOutlet UIImageView *star;
+    IBOutlet UIImageView *star_shadow;
+    IBOutlet UIImageView *paddle_red;
+    IBOutlet UIImageView *paddle_blue;
+    
+    IBOutlet UILabel *player_score;
+    IBOutlet UILabel *computer_score;    
+    IBOutlet UILabel *start_message;
+    
+    CGPoint star_velocity;
+    NSInteger game_state;
+}
+
+@property(strong, nonatomic) IBOutlet UIImageView *star;
+@property(strong, nonatomic) IBOutlet UIImageView *star_shadow;
+@property(strong, nonatomic) IBOutlet UIImageView *paddle_red;
+@property(strong, nonatomic) IBOutlet UIImageView *paddle_blue;
+
+@property(strong, nonatomic) IBOutlet UILabel *start_message;
+@property(strong, nonatomic) IBOutlet UILabel *player_score;
+@property(strong, nonatomic) IBOutlet UILabel *computer_score;
+
+@property(nonatomic) CGPoint star_velocity;
+@property(nonatomic) NSInteger game_state;
 
 @end
--- pong/ViewController.m
+++ pong/ViewController.m
@@ -8,7 +8,27 @@
 
 #import "ViewController.h"
 
+#define GAME_STATE_PAUSED   1
+#define GAME_STATE_RUNNING  2
+
+#define STAR_SPEED_X 6
+#define STAR_SPEED_Y 6
+
+#define COMP_MOVE_SPEED 3
+
+#define SCORE_TO_WIN 3
+
 @implementation ViewController
+
+@synthesize star;
+@synthesize star_shadow;
+@synthesize paddle_red;
+@synthesize paddle_blue;
+@synthesize player_score;
+@synthesize computer_score;
+@synthesize star_velocity;
+@synthesize game_state;
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -18,10 +38,101 @@
 
 #pragma mark - View lifecycle
 
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [[event allTouches] anyObject];
+    CGPoint location = [touch locationInView:touch.view];
+
+    paddle_red.center = CGPointMake(location.x, paddle_red.center.y);
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    if(game_state == GAME_STATE_PAUSED) {
+        start_message.hidden = YES;
+        game_state = GAME_STATE_RUNNING;
+    } else if(game_state == GAME_STATE_RUNNING) {
+        [self touchesMoved:touches withEvent:event];
+    }
+}
+
+- (void)gameLoop {
+    if(game_state == GAME_STATE_RUNNING) {
+        star.center = CGPointMake(star.center.x + star_velocity.x, star.center.y + star_velocity.y);
+        star_shadow.center = CGPointMake(star.center.x + star_velocity.x, star.center.y + star_velocity.y);
+        
+        if(star.center.x > self.view.bounds.size.width || star.center.x < 0) {
+            star_velocity.x = -star_velocity.x;     
+        }
+        if(star.center.y > self.view.bounds.size.height || star.center.y < 0) {
+            star_velocity.y = -star_velocity.y;
+        }
+        
+        if(CGRectIntersectsRect(star.frame, paddle_blue.frame)) {
+            if(star.center.y > paddle_blue.center.y) {
+                NSLog(@"%fl", star_velocity.x);
+                star_velocity.y = -star_velocity.y;
+                NSLog(@"%fl %fl", star.center.x, paddle_blue.center.x);
+                
+                
+                if(star_velocity.x) {
+                    // Left to Right -->
+                    if(star.center.x > paddle_blue.center.x) {
+                        star_velocity.x = -star_velocity.x;
+                    }
+                } else {
+                    // Right to Left <--
+                    if(star.center.x < paddle_blue.center.x) {
+                        star_velocity.x = -star_velocity.x;
+                    }
+                }
+
+            }
+        }
+        
+        if(CGRectIntersectsRect(star.frame, paddle_red.frame)) {
+            if(star.center.y < paddle_red.center.y) {
+                NSLog(@"%fl", star_velocity.x);
+                star_velocity.y = -star_velocity.y;
+                NSLog(@"%fl %fl", star.center.x, paddle_red.center.x);
+                
+
+                if(star_velocity.x) {
+                    // Left to Right -->
+                    if(star.center.x > paddle_red.center.x) {
+                        star_velocity.x = -star_velocity.x;
+                    }
+                } else {
+                    // Right to Left <--
+                    if(star.center.x < paddle_red.center.x) {
+                        star_velocity.x = -star_velocity.x;
+                    }
+                }
+            }
+        }
+        
+        // Computer side of the court
+        if(star.center.y <= self.view.center.y) {
+            if(star.center.x > paddle_blue.center.x) {
+                paddle_blue.center = CGPointMake(paddle_blue.center.x + COMP_MOVE_SPEED, paddle_blue.center.y);
+            }
+            if(star.center.x < paddle_blue.center.x) {
+                paddle_blue.center = CGPointMake(paddle_blue.center.x - COMP_MOVE_SPEED, paddle_blue.center.y);
+            }
+
+        }
+    } else {
+        if(start_message.hidden) {
+            start_message.hidden = NO;
+        }
+    }
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    self.game_state = GAME_STATE_PAUSED;
+    star_velocity = CGPointMake(STAR_SPEED_X, STAR_SPEED_Y);
+    [NSTimer scheduledTimerWithTimeInterval:0.02 target:self selector:@selector(gameLoop) userInfo:nil repeats:YES];
 }
 
 - (void)viewDidUnload
